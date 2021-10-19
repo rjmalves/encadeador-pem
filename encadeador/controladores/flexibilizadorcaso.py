2706b18e208e5a151d7cfe2a8325c90cb8a46637
Rogerio Alves <rogerioalves.ee@gmail.com>
2021-10-19 10:38:38 -0300

    Flexibilizador lendo arquivos

--- encadeador/controladores/flexibilizadorcaso.py
+++ encadeador/controladores/flexibilizadorcaso.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
 from logging import Logger
+from idecomp.decomp.inviabunic import InviabUnic  # type: ignore
+from idecomp.decomp.dadger import Dadger  # type: ignore
 
 from encadeador.modelos.caso import Caso, CasoDECOMP
 
@@ -39,8 +41,13 @@ class FlexibilizadorDECOMP(Flexibilizador):
         self._log.info(f"Flexibilizando caso {self._caso.nome}: " +
                        f"{self._caso.numero_flexibilizacoes} de {max_flex}")
         self._caso.adiciona_flexibilizacao()
-        # TODO - Lê o inviab_unic.rvX
-        # TODO - Lê o dadger.rvX
+        # Lê o inviab_unic.rvX
+        arq_inviab = f"inviab_unic.rv{self._caso.revisao}"
+        inviab = InviabUnic.le_arquivo(self._caso.caminho, arq_inviab)
+        # Lê o dadger.rvX
+        arq_dadger = f"dadger.rv{self._caso.revisao}"
+        dadger = Dadger.le_arquivo(self._caso.caminho, arq_dadger)
         # TODO - Faz as flexibilizações
-        # TODO - Escreve o dadger.rvX
+        # Escreve o dadger.rvX de saída
+        dadger.escreve_arquivo(self._caso.caminho, arq_dadger)
         return True
