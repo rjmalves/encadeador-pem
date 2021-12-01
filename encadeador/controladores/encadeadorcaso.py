2dcea57238c935dd1998af8e741ca665ab7a479d
Rogerio Alves <rogerioalves.ee@gmail.com>
2021-12-01 15:43:24 -0300

    Debug GNL

--- encadeador/controladores/encadeadorcaso.py
+++ encadeador/controladores/encadeadorcaso.py
@@ -467,11 +467,13 @@ class EncadeadorDECOMPDECOMP(Encadeador):
                     nome = mapa_codigo_usina[c]
                     filtro = (op["Usina"] == nome) & (op["Início Semana"] == data)
                     geracoes = op.loc[filtro, cols_despacho]
+                    self._log.info(f"Encadeando GNL: {[g for g in geracoes.to_numpy()]}")
                     r.geracoes = [g for g in geracoes.to_numpy()]
                 else:
                     # Procura pelo registro anterior com a mesma data
                     reg_ant = [ra for ra in registros_usina_anterior
                                if ra.dados[-1] == r.dados[-1]][0]
+                    self._log.info(f"Encadeando GNL: {reg_ant.geracoes}")
                     r.geracoes = reg_ant.geracoes
 
         # Escreve o arquivo de saída
