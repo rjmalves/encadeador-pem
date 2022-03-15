5f5a2026a14b9ffd54777e63edd1695798f32254
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-03-15 11:01:26 -0300

    Fix inicializacao estudo

--- encadeador/app.py
+++ encadeador/app.py
@@ -20,7 +20,7 @@ class App:
     def __constroi_estudo_encadeado(self):
         self._estudo = ArmazenadorEstudo.gera_estudo()
         self._monitor = MonitorEstudo(self._estudo)
-        self._estudo.atualiza(EstadoEstudo.NAO_INICIADO, True)
+        self._estudo.atualiza(EstadoEstudo.EXECUTANDO, True)
         sintetizador = SintetizadorEstudo(self._estudo)
         if not sintetizador.sintetiza_estudo():
             raise RuntimeError()
