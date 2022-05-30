b1bb8beeffa2029a1476c6cf62b14843b615baa4
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-05-30 16:38:51 -0300

    Remove print teste

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from encadeador.app import App
 from encadeador.modelos.configuracoes import Configuracoes
 from encadeador.utils.log import Log
 
-print("teste")
-
 # Lê as configurações das variáveis de ambiente
 load_dotenv(override=True)
 
