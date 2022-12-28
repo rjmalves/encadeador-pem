c38cb6754a635b3877c1c169bbdd753d28173eb9
Rogerio <rogerioalves.ee@gmail.com>
2022-12-28 13:37:58 -0300

    fix inicia db

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 from encadeador.adapters.orm import registry
 from sqlalchemy import create_engine
 from encadeador.adapters.orm.util import start_mappers
+from encadeador.utils.log import Log
 
 
 def start_db(caminho: str):
-    SQLITE_URL = f"sqlite:///{caminho}data.db"
+    SQLITE_URL = f"sqlite:///{caminho}/data.db"
+    Log.log().info(f"Inicializando DB em {SQLITE_URL}")
     engine = create_engine(SQLITE_URL)
     registry.metadata.create_all(engine)
     start_mappers()
